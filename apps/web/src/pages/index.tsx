056280f0f6ff48ed79e5f3a9133fa12042aa72a9
Christoph <chris.schroeder254@gmail.com>
2025-12-07 14:35:05 +0100

    Removed vite and configured next properly

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
+import App from '../App';
 
 export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Next.js app is set up</h1>
-      <p>CI will build this with turbo.</p>
-    </main>
-  );
+  return <App />;
 }
